48eb300684d7a33b4b75878d1a0619839ab2306a
o1073011 <65273464+o1073011@users.noreply.github.com>
2020-06-24 00:59:19 +0800

    Diary 1-3

--- app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/SplashActivity.java
+++ app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/SplashActivity.java
@@ -2,13 +2,26 @@ package tw.pu.edu.gm.o1073011.lifetracker;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashActivity extends AppCompatActivity {
 
+    Handler handler;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        handler=new Handler();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    Intent intent=new Intent(SplashActivity.this,MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                }
+            },3000);
     }
 }
