e4e53cc58946e052109ff0438bd9c37287239c39
o1073011 <65273464+o1073011@users.noreply.github.com>
2020-06-24 05:48:15 +0800

    No Note Page

--- app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/HomeActivity.java
+++ app/src/main/java/tw/pu/edu/gm/o1073011/lifetracker/HomeActivity.java
@@ -11,10 +11,12 @@ import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentTransaction;
 
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.FrameLayout;
 
 import com.google.android.material.bottomnavigation.BottomNavigationMenu;
